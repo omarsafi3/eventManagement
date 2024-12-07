4636055b339b57369746d8d2d273da8ea2b9a6bc
omarsafi3 <safiomar@isimsf.u-sfax.tn>
2024-12-07 16:05:12 +0100

    .

--- eventManagement/src/main/java/org/example/eventmanagement/config/WebConfig.java
+++ eventManagement/src/main/java/org/example/eventmanagement/config/WebConfig.java
@@ -9,11 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // Allow requests from this origin
+        // Allow all origins for testing (change it to specific origins in production)
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:4200")  // Angular dev server URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);  // Allow sending credentials (like cookies or tokens)
     }
 }
